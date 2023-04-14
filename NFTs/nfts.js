93794f086c954c541d4a7a67427be0052102ea65
Edie-MinwooPark <alsdn8055@gmail.com>
2023-04-14 15:41:45 +0900

    스와이프 부분 완성

--- NFTs/nfts.js
+++ NFTs/nfts.js
@@ -11,7 +11,7 @@ let {length} = document.querySelectorAll('.swiper ul li');
 
 
 //스와이프 함수 구현
-
+let backgroundchanger = document.querySelector('.nfts-swiper');
 function swiperMove(){
     _swiperContent.style.left = -(_index * _swiperWidth) + 'px';
 };
@@ -24,12 +24,26 @@ _prev.addEventListener('click',function(){
     console.log("ddd")
     if(_index>0){
     _index--;
+    if(_index == 3){
+        backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,gray)'
+
+    }
+    if(_index == 2){
+        backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,#B08F89)'
+    }
+    if(_index == 4 || _index == 0){
+        backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,antiquewhite)'
+    }
+    if(_index == 5|| _index == 1){
+        backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,pink)'
+    }
     }
     swiperMove();
     _swiperContent.ontransitionend =function(){
         if(_index === 0){
             _swiperContent.style.transition = 'none'
             _index = 4
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,antiquewhite)'
             swiperMove();
         }
         isActive = false;
@@ -44,16 +58,84 @@ _next.addEventListener('click',function(){
     _swiperContent.style.transition = '1s'
     if(_index < length-1){
         _index ++;
+        if(_index == 3){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,gray)'
+
+        }
+        if(_index == 2){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,#B08F89)'
+        }
+        if(_index == 4){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,antiquewhite)'
+        }
+        if(_index == 5 || _index == 1){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,pink)'
+        }
     }
     swiperMove();
     _swiperContent.ontransitionend = function(){
         if(_index === length -1){
             _swiperContent.style.transition = 'none'
             _index = 1
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white, pink)'
             swiperMove();
         }
         isActive = false;
     }
 })
 
+//4초에 한번씩 자동으로 스와이프 구현
+setInterval(function(){
+    _swiperContent.style.transition = '1s'
+    if(_index < length-1){
+        _index ++;
+        
+        if(_index == 3){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,gray)'
 
+        }
+        if(_index == 2){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,#B08F89)'
+        }
+        if(_index == 4){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,antiquewhite)'
+        }
+        if(_index == 5){
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white,pink)'
+        }
+    }
+    swiperMove();
+    _swiperContent.ontransitionend = function(){
+        if(_index === length -1){
+            _swiperContent.style.transition = 'none'
+            _index = 1
+            backgroundchanger.style.backgroundImage = 'linear-gradient(to top, white, pink)'
+            swiperMove();
+        }
+        isActive = false;
+    }
+},4000);
+
+
+
+
+// 스와이퍼 클릭시 실제 링크로 이동
+
+let swiper = document.querySelector('.swiper');
+
+swiper.addEventListener('click',function(e){
+    e.stopPropagation();
+    if(_index == 1 || _index == 5){
+        window.location.href = 'https://app.uniswap.org/#/nfts/collection/0xed5af388653567af2f388e6224dc7c4b3241c544'
+    }
+    if(_index == 2){
+        window.location.href = 'https://app.uniswap.org/#/nfts/collection/0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'
+    }
+    if(_index == 3){
+        window.location.href = 'https://app.uniswap.org/#/nfts/collection/0x60e4d786628fea6478f785a6d7e704777c86a7c6'
+    }
+    if(_index == 0 || _index == 4){
+        window.location.href = 'https://app.uniswap.org/#/nfts/collection/0xf48b45479ba7e121a3542626d5a5f3b07ec5b65d'
+    }
+    
+});
