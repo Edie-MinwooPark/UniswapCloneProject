71cf52ec64006832f1d2b13af0b644d6120de1dd
jiwoog0 <nikejin0920@gmail.com>
2023-04-17 14:12:01 +0900

    3번째

--- index.js
+++ index.js
@@ -1,34 +1,94 @@
-function drawer(el, open = false){
+// function drawer(el, open = false){
+//   this.el = el;
+//   this.isOpen = open;
+
+// Object.assign(this.el.style,{
+//   display : "block",
+//   position : "fixed",
+//   top : 0,
+//   bottom : 0,
+//   right : 0,
+//   width : "340px",
+//   paddingLeft : "30px",
+//   backgroundColor : "rgba(255,255,255,0.5)",
+//   transition : "1s",
+//   borderRadius: "10px"
+// });
+// }
+// drawer.prototype.open = function(){
+//   this.isOpen = true;
+//   this.el.style.transform = "translate(0)";
+// }
+// drawer.prototype.close = function(){
+//   this.isOpen = false;
+//   this.el.style.transform = "translate(500px)";
+// }
+// const sideMenu = new drawer(document.querySelector('.drawer'));
+// sideMenu.close();
+// document.querySelector('.drawer-opener').onclick = function(){
+//   if(!sideMenu.isOpen)sideMenu.open();
+//   else sideMenu.close();
+// }
+function drawer(el, open = false) {
   this.el = el;
   this.isOpen = open;
+  this.closeButton = null;
 
-Object.assign(this.el.style,{
-  display : "block",
-  position : "fixed",
-  top : 0,
-  bottom : 0,
-  right : 0,
-  width : "340px",
-  paddingLeft : "30px",
-  backgroundColor : "rgba(255,255,255,0.5)",
-  transition : "1s",
-  borderRadius: "10px"
-});
+  Object.assign(this.el.style, {
+    display: "block",
+    position: "fixed",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    width: "340px",
+    paddingLeft: "30px",
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    transition: "1s",
+    borderRadius: "10px",
+  });
+  
+  this.addCloseButton();
 }
-drawer.prototype.open = function(){
+
+drawer.prototype.open = function () {
   this.isOpen = true;
   this.el.style.transform = "translate(0)";
-}
-drawer.prototype.close = function(){
+};
+
+drawer.prototype.close = function () {
   this.isOpen = false;
   this.el.style.transform = "translate(500px)";
-}
-const sideMenu = new drawer(document.querySelector('.drawer'));
+};
+
+drawer.prototype.addCloseButton = function () {
+  // closeButton 프로퍼티 초기화
+  this.closeButton = document.createElement("div");
+  this.closeButton.innerText = ">>";
+  Object.assign(this.closeButton.style, {
+    position: "absolute",
+    top: "27px",
+    right: "340px",
+    cursor: "pointer",
+    fontSize: "1.5rem",
+    fontWeight: "bold",
+  });
+
+  // closeButton을 드로어에 추가
+  this.el.appendChild(this.closeButton);
+
+  this.closeButton.addEventListener("click", () => {
+    this.close();
+  });
+};
+
+const sideMenu = new drawer(document.querySelector(".drawer"));
 sideMenu.close();
-document.querySelector('.drawer-opener').onclick = function(){
-  if(!sideMenu.isOpen)sideMenu.open();
+
+document.querySelector(".drawer-opener").onclick = function () {
+  if (!sideMenu.isOpen) sideMenu.open();
   else sideMenu.close();
-}
+};
+
 
 // document.querySelector(".learnmore").onclick
 
@@ -47,3 +107,43 @@ popupButton.onclick = function() {
 bg.onclick = function(){
   popup.style.display = "none";
 }
+// -------------------------------------------------------------------
+
+// const searchbox = document.querySelector('.searchBar');
+// const searchContent = document.querySelector('.searchpopup');
+// const dg = document.querySelector(".dg")
+
+// searchbox.onclick = function(){
+//   if(getComputedStyle(searchContent).display == "block"){
+//     searchContent.style.display = "none";
+//   }else
+//     searchContent.style.display = "block";
+//   }
+//   dg.onclick = function(){
+//     searchContent.style.display = "none";
+//   }
+//   window.onbeforeunload = function() {
+//     if (searchContent.style.display == "block") {
+//       searchContent.style.display = "none";
+//     }
+//   }
+const searchbox = document.querySelector('.searchBar');
+const searchContent = document.querySelector('.searchpopup');
+const dg = document.querySelector(".dg")
+
+searchbox.onclick = function(){
+  if(getComputedStyle(searchContent).display == "block"){
+    searchContent.style.display = "none";
+  }else
+    searchContent.style.display = "block";
+  }
+  
+dg.onclick = function(){
+  searchContent.style.display = "none";
+}
+
+window.onload = function() {
+  searchContent.style.display = "none";
+}
+
+
