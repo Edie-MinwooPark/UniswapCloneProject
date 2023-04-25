aa4d0ac13fd3e4ec696948ea0c1ae1aa6ae4fbe9
jinwhan804 <jinwhan804@hanmail.net>
2023-04-25 10:55:57 +0900

    legal 팝업 수정

--- Admin/admin.js
+++ Admin/admin.js
@@ -431,3 +431,22 @@ function render(){
 
     }
 }
+
+// -----------------legal 버튼 -----------------------------------------
+let legalinpopup = document.querySelector('.legalinpopup');
+let legalBtn = document.querySelector('.legalBtn');
+let legalBox = document.querySelector('.legalContainer');
+let home = document.querySelector('.Home');
+
+legalinpopup.onclick = function(){
+  legalBox.classList.add('active');
+  // home.style.backgroundColor = "black";
+  home.style.opacity = "0.4";
+  document.body.style.overflow = "hidden";
+}
+legalBtn.onclick = function(){
+  legalBox.classList.remove('active');
+  // home.style.backgroundColor = "rgb(255,205,210)";
+  home.style.opacity = "1";
+  document.body.style.overflow = "auto";
+}
